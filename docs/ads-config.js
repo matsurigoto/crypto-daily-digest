099cafcfab3a3b8c26970ca120990063b3108af1
Duran Hsieh <dog0416@gmail.com>
2026-03-01 23:01:11 +0800

    Enable ads and set Google Adsense publisher ID

--- docs/ads-config.js
+++ docs/ads-config.js
@@ -1,7 +1,7 @@
 /* ads-config.js – Google Adsense 廣告設定 */
 window.ADS_CONFIG = {
-  enabled: false,                    // 設為 true 以啟用廣告
-  client: '',                        // 填入 Google Adsense 發佈者 ID，例如 'ca-pub-XXXXXXXXXXXXXXXX'
+  enabled: true,                    // 設為 true 以啟用廣告
+  client: 'ca-pub-5065042543576800',                        // 填入 Google Adsense 發佈者 ID，例如 'ca-pub-XXXXXXXXXXXXXXXX'
   slots: {
     header:  '',                     // Header 下方橫幅廣告 slot ID
     sidebar: '',                     // 內容間穿插廣告 slot ID
